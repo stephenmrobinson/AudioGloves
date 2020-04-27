b21f62e2bc0abac988aa0be95293814e67799d6b
hackerrank <cs.smrobinson@gmail.com>
2020-04-26 20:13:54 -0400

    added inputs for all five sensors as well as new pitch encoding

--- mvp_wiring.ino
+++ mvp_wiring.ino
@@ -7,9 +7,15 @@ int fsrAnalogPin1 = 1; //L1 or Left Finger 1
 int fsrAnalogPin2 = 2; //L2
 int fsrAnalogPin3 = 3; //L3
 int fsrAnalogPin4 = 4; //L4
+int fsrAnalogPin6 = 6; //L5
 
 int fsrReading0;
 int fsrReading1;
+int fsrReading2;
+int fsrReading3;
+int fsrReading6;
+
+int pitchEncoding;
 
 void setup() {
   
@@ -37,22 +43,53 @@ void loop() {
 
   fsrReading0 = analogRead(fsrAnalogPin0);
   fsrReading1 = analogRead(fsrAnalogPin1);
-  //Serial.print("Analog readings--- Pin 0 = ");
+  fsrReading2 = analogRead(fsrAnalogPin2);
+  fsrReading3 = analogRead(fsrAnalogPin3);
+  fsrReading6 = analogRead(fsrAnalogPin6);
 
-  if(fsrReading0 > 700) {
-    Serial.println("1");
+  pitchEncoding = 0;
+  if(fsrReading0 > 700){
+    pitchEncoding += 1;
   }
 
   if(fsrReading1 > 700){
-    Serial.println("2");
+    pitchEncoding += 2;
   }
 
-  if(fsrReading1 <700 & fsrReading0 < 700){
-    Serial.println("0");
+  if(fsrReading2 > 700){
+    pitchEncoding += 4;
   }
+
+  if(fsrReading3 > 700){
+    pitchEncoding += 8;
+  }
+
+  if(fsrReading6 > 700){
+    pitchEncoding += 16;
+  }
+
+  Serial.println(pitchEncoding);
+
+//  if(fsrReading0 > 700) {
+//    Serial.println("1");
+//  }
+//
+//  if(fsrReading1 > 700){
+//    Serial.println("2");
+//  }
+//
+//  if(fsrReading2 > 700){
+//    Serial.println("3");
+//  }
+//
+//  if(fsrReading1 <700 & fsrReading0 < 700 & fsrReading2 < 0){
+//    Serial.println("0");
+//  }
 //  Serial.println(fsrReading0);
 //  //Serial.print(" Pin 1 = ");
 //  Serial.println(fsrReading1);
+//  Serial.println(fsrReading2);
+
 
 
 
